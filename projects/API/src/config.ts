d35cf9b88cdebbe5edacb554708b427f7aecc336
Jhoanna Pereira <123725071+PJAne24@users.noreply.github.com>
2024-02-20 14:20:41 +0100

    Segundo commit

--- projects/API/src/config.ts
+++ projects/API/src/config.ts
@@ -11,7 +11,7 @@ export default {
     MONGO_DATABASE: process.env.MONGO_DATABASE || 'productos', //Accedemos a env y se ejecutara la variable si esta definida o sino 'productos'
     MONGO_USER: process.env.MONGO_USER || 'admin',
     MONGO_PASSWORD: process.env.MONGO_PASSWORD ||  'admin',
-    MONGO_HOST: process.env.MONGO_HOST || 'localhost',
+    MONGO_HOST: process.env.MONGO_HOST || '127.0.0.1',
     PORT: process.env.PORT || 3000 //que utilice un puerto en env o el 3000
 }
     
